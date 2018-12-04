873a1de621e20c571c5483a1bc7abe4244fb871a
Kaan <kaan94karaca@gmail.com>
2018-12-04 17:35:29 +0300

    parrot trouble

--- warmup-1.py
+++ warmup-1.py
@@ -24,3 +24,11 @@ Given an int n, return the absolute difference between n and 21, except return d
 """
 def diff21(n):
     return abs(21-n) if n<21 else abs(21-n)*2
+
+
+"""
+We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble.
+"""
+def parrot_trouble(talking, hour):
+    return talking and (hour < 7 or hour > 20)
+    #return talking and hour not in (7,21)
