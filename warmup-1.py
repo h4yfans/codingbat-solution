07ffdacbb3fe4003878e41457fe9fd44fd73849e
Kaan <kaan94karaca@gmail.com>
2018-12-04 20:38:13 +0300

    front3

--- warmup-1.py
+++ warmup-1.py
@@ -75,3 +75,10 @@ Given a string, return a new string where the first and last chars have been exc
 """
 def front_back(str):
     return str if len(str) < 2 else str[-1] + str[1:len(str)-1] + str[0]
+
+
+"""
+Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+"""
+def front3(str):
+      return str[:3]*3
