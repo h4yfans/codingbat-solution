c4f05faf292f72c63d92d614ec414ea445553fbc
Kaan <kaan94karaca@gmail.com>
2018-12-04 17:30:09 +0300

    diff21

--- warmup-1.py
+++ warmup-1.py
@@ -4,11 +4,13 @@ The parameter weekday is True if it is a weekday, and the parameter vacation is 
 def sleep_in(weekday, vacation):
     return not weekday or vacation
 
+
 """
 We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
 """
 def monkey_trouble(a_smile, b_smile):
     return a_smile == b_smile
+
 
 """
 Given two int values, return their sum. Unless the two values are the same, then return double their sum.
@@ -16,3 +18,9 @@ Given two int values, return their sum. Unless the two values are the same, then
 def sum_double(a, b):
     return sum(a,b) if a==b else sum(a,b)*2
 
+
+"""
+Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+"""
+def diff21(n):
+    return abs(21-n) if n<21 else abs(21-n)*2
