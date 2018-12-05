8b8c594971ee7bf3ce7e0086d6c973605c178028
Kaan <kaan94karaca@gmail.com>
2018-12-05 19:11:34 +0300

    make_ends

--- list-1.py
+++ list-1.py
@@ -72,3 +72,10 @@ Given 2 int arrays, a and b, each length 3, return a new array length 2 containi
 """
 def middle_way(a, b):
     return [a[1],b[1]]
+
+
+"""
+Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
+"""
+def make_ends(nums):
+    return [nums[0], nums[-1]]
