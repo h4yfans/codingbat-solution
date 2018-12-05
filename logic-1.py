7a3f974c4656058be0d06c09d1d2a60197dac315
Kaan <kaan94karaca@gmail.com>
2018-12-06 01:36:16 +0300

    caught_speeding

--- logic-1.py
+++ logic-1.py
@@ -32,3 +32,19 @@ Given an int temperature and a boolean is_summer, return True if the squirrels p
 """
 def squirrel_play(temp, is_summer):
     return temp in range(60, 101 if is_summer else 91)
+
+
+"""
+You are driving a little too fast, and a police officer stops you.
+Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket.
+If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1.
+If speed is 81 or more, the result is 2. Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
+"""
+def caught_speeding(speed, is_birthday):
+    speeding = speed - (65 if is_birthday else 60)
+    if speeding > 20:
+        return 2
+    elif speeding > 0:
+        return 1
+    else:
+        return 0
