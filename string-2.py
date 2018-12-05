16e57ba161ac9eff4d775022b667d728fccfd101
Kaan <kaan94karaca@gmail.com>
2018-12-05 17:52:23 +0300

    xyz_there

--- string-2.py
+++ string-2.py
@@ -48,3 +48,17 @@ def end_other(a, b):
     return longest[-len(shortest):] == shortest
     #return longest.endswith(shortest)
 
+
+"""
+Return True if the given string contains an appearance of "xyz" where the
+xyz is not directly preceeded by a period (.). So "xxyz" counts but "x.xyz" does not.
+"""
+def xyz_there(str):
+    if str[:3] == 'xyz':
+        return True
+
+    for i in range(1, len(str)-2):
+        if str[i-1] != "." and str[i:i+3] == "xyz":
+            return True
+
+    return False
