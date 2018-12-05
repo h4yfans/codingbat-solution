6e7940361fe18c619e2f29969ffe98ace045d6a6
Kaan <kaan94karaca@gmail.com>
2018-12-05 17:55:12 +0300

    same_first_last

--- list-1.py
+++ list-1.py
@@ -4,3 +4,11 @@ The array will be length 1 or more.
 """
 def first_last6(nums):
     return nums[0] == 6 or nums[-1] == 6
+
+
+"""
+Given an array of ints, return True if the array is length 1 or more,
+and the first element and the last element are equal.
+"""
+def same_first_last(nums):
+    return len(nums) > 0 and nums[0] == nums[-1]
