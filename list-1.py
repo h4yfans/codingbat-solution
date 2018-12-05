87e5596c4fc3a0958263ab2bfea33fffb5331d5a
Kaan <kaan94karaca@gmail.com>
2018-12-05 19:03:25 +0300

    max_end3

--- list-1.py
+++ list-1.py
@@ -48,3 +48,15 @@ Given an array of ints length 3, return a new array with the elements in reverse
 """
 def reverse3(nums):
     return [nums[2], nums[1], nums[0]]
+
+"""
+Given an array of ints length 3, figure out which is larger, the first or last element in the array,
+and set all the other elements to be that value. Return the changed array.
+"""
+def max_end3(nums):
+    max_num = max(nums[0], nums[2])
+    new_list = list(map(lambda x: max_num, nums))
+
+    return new_list
+
+
