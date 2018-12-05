ec1e233e2b2904e25405b0a13e13a97d4931b2a7
Kaan <kaan94karaca@gmail.com>
2018-12-05 16:24:29 +0300

    combo string

--- string-1.py
+++ string-1.py
@@ -59,3 +59,12 @@ so "Hello" yields "ell". The string length will be at least 2.
 """
 def without_end(str):
     return str[1:-1]
+
+
+"""
+Given 2 strings, a and b, return a string of the form short+long+short,
+with the shorter string on the outside and the longer string on the inside.
+The strings will not be the same length, but they may be empty (length 0).
+"""
+def combo_string(a, b):
+    return b+a+b if len(b) < len(a) else a+b+a
