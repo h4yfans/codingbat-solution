3ad9ab7ca5acfa440ebda03c75bfd91f817325aa
Kaan <kaan94karaca@gmail.com>
2018-12-05 15:55:57 +0300

    make_abba

--- string-1.py
+++ string-1.py
@@ -3,3 +3,11 @@ Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
 """
 def hello_name(name):
     return 'Hello {}!'.format(name)
+
+
+"""
+Given two strings, a and b, return the result of putting them together
+in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+"""
+def make_abba(a, b):
+    return a+b+b+a
