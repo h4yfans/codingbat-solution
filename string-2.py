0db37b3ea69729fa715e58e8f3d2604abb5a05e3
Kaan <kaan94karaca@gmail.com>
2018-12-05 17:18:49 +0300

    end_other

--- string-2.py
+++ string-2.py
@@ -34,3 +34,17 @@ def count_code(str):
             count_d += 1
     return count_code + count_d
 
+
+"""
+Given two strings, return True if either of the strings appears at the very end of the other string,
+ignoring upper/lower case differences (in other words, the computation should not be "case sensitive").
+Note: s.lower() returns the lowercase version of a string.
+"""
+def end_other(a, b):
+    a = a.lower()
+    b = b.lower()
+    longest, shortest = (a, b) if len(a) > len(b) else (b,a)
+
+    return longest[-len(shortest):] == shortest
+    #return longest.endswith(shortest)
+
