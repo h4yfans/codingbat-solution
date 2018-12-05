16fc574c2143b9a905ba7999d7ee97414af579db
Kaan <kaan94karaca@gmail.com>
2018-12-05 18:41:40 +0300

    reverse3

--- list-1.py
+++ list-1.py
@@ -41,3 +41,10 @@ Given an array of ints length 3, return an array with the elements "rotated left
 """
 def rotate_left3(nums):
    return [nums[1], nums[2], nums[0]]
+
+
+"""
+Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+"""
+def reverse3(nums):
+    return [nums[2], nums[1], nums[0]]
