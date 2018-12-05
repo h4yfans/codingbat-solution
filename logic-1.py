b233580976765dd123b7dc24d5d58c4bd2a1b01c
Kaan <kaan94karaca@gmail.com>
2018-12-06 01:45:15 +0300

    sorta_sum

--- logic-1.py
+++ logic-1.py
@@ -48,3 +48,11 @@ def caught_speeding(speed, is_birthday):
         return 1
     else:
         return 0
+
+
+"""
+Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive,
+are forbidden, so in that case just return 20.
+"""
+def sorta_sum(a, b):
+    return a+b if not (a+b in range(10, 20)) else 20
