933a5d718104cda3d4e1b0d749b686a704e0da3f
Kaan <kaan94karaca@gmail.com>
2018-12-05 16:02:28 +0300

    first_two

--- string-1.py
+++ string-1.py
@@ -35,3 +35,12 @@ last 2 chars of the original string. The string length will be at least 2.
 """
 def extra_end(str):
     return str[-2:]*3
+
+
+"""
+Given a string, return the string made of its first two chars, so the String "Hello"
+yields "He". If the string is shorter than length 2, return whatever there is, so "X"
+yields "X", and the empty string "" yields the empty string "".
+"""
+def first_two(str):
+    return str[:2] if len(str) > 2 else str
