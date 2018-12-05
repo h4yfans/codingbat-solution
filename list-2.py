7a3cc9ee29f5a641da78405a8376b6ef0e5f4f29
Kaan <kaan94karaca@gmail.com>
2018-12-05 22:56:02 +0300

    sum13 fix

--- list-2.py
+++ list-2.py
@@ -40,7 +40,7 @@ come immediately after a 13 also do not count.
 def sum13(nums):
     while 13 in nums:
         if nums.index(13) < len(nums)-1:
-            nums.pop(nums.index(13))
-        nums.pop(nums.index(13)+1)
+            nums.pop(nums.index(13)+1)
+        nums.pop(nums.index(13))
 
     return sum(nums)
