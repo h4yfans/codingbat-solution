9d3fbae0bb2214685f91a3edb1468d12ec42136b
Kaan <kaan94karaca@gmail.com>
2018-12-06 02:10:23 +0300

    love6

--- logic-1.py
+++ logic-1.py
@@ -68,3 +68,13 @@ def alarm_clock(day, vacation):
     pronto = "7:00" if not vacation else "10:00"
     tarde = "10:00" if not vacation else "off"
     return pronto if day not in [6,0] else tarde
+
+
+
+"""
+The number 6 is a truly great number. Given two int values, a and b,
+return True if either one is 6. Or if their sum or difference is 6.
+Note: the function abs(num) computes the absolute value of a number.
+"""
+def love6(a, b):
+      return a == 6 or b == 6 or a+b == 6 or abs(a-b) == 6
