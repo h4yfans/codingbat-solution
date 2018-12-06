906e1eaf9383921eef15f7006f24a68cb8b631ef
Kaan <kaan94karaca@gmail.com>
2018-12-06 03:07:36 +0300

    lone_sum

--- logic-2.py
+++ logic-2.py
@@ -8,3 +8,21 @@ def make_bricks(small, big, goal):
     resto = goal
     resto -= 5*min(big,goal/5)
     return resto-small <= 0
+
+
+"""
+Given 3 int values, a b c, return their sum.
+However, if one of the values is the same as another of the values,
+it does not count towards the sum.
+"""
+def lone_sum(a, b, c):
+    if a == b and b == c and c == a:
+        return 0
+    if a == b:
+        return c
+    if b == c:
+        return a
+    if c == a:
+        return b
+    return a+b+c
+
