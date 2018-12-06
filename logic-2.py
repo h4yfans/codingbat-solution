3b91b298ac01f9dc07628cb73be41cd5c07294c9
Kaan <kaan94karaca@gmail.com>
2018-12-06 03:14:20 +0300

    lucky_sum

--- logic-2.py
+++ logic-2.py
@@ -26,3 +26,12 @@ def lone_sum(a, b, c):
         return b
     return a+b+c
 
+
+"""
+Given 3 int values, a b c, return their sum.
+However, if one of the values is 13 then it does not count towards the sum and values to its right do not count.
+So for example, if b is 13, then both b and c do not count.
+"""
+def lucky_sum(a, b, c):
+    list = [a, b, c]
+    return sum(list[:list.index(13)]) if 13 in list else a+b+c
