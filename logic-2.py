edf56440dc73074f722eccec8863825e29ef6fea
Kaan <kaan94karaca@gmail.com>
2018-12-06 03:17:37 +0300

    no_teen_sum

--- logic-2.py
+++ logic-2.py
@@ -35,3 +35,16 @@ So for example, if b is 13, then both b and c do not count.
 def lucky_sum(a, b, c):
     list = [a, b, c]
     return sum(list[:list.index(13)]) if 13 in list else a+b+c
+
+
+"""
+Given 3 int values, a b c, return their sum.
+However, if any of the values is a teen -- in the range 13..19 inclusive -- then that value counts as 0,
+except 15 and 16 do not count as a teens. Write a separate helper "def fix_teen(n):"that takes in an int
+value and returns that value fixed for the teen rule. In this way, you avoid repeating the teen code 3 times
+(i.e. "decomposition"). Define the helper below and at the same indent level as the main no_teen_sum().
+"""
+def no_teen_sum(a, b, c):
+    def fix_teen(n):
+        return n if n not in [13, 14, 17, 18, 19] else 0
+    return fix_teen(a)+fix_teen(b)+fix_teen(c)
