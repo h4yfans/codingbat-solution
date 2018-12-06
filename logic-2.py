704d60dcee9c92d767463a32503c60069f2b4cf6
Kaan <kaan94karaca@gmail.com>
2018-12-06 03:26:42 +0300

    make_chocolate

--- logic-2.py
+++ logic-2.py
@@ -76,3 +76,16 @@ def close_far(a, b, c):
         (a_b_diff <= 1 and a_c_diff >= 2 and b_c_diff >= 2) !=
         (a_c_diff <= 1 and a_b_diff >= 2 and b_c_diff >= 2)
         )
+
+"""
+We want make a package of goal kilos of chocolate.
+We have small bars (1 kilo each) and big bars (5 kilos each).
+Return the number of small bars to use, assuming we always use
+big bars before small bars. Return -1 if it can't be done.
+"""
+def make_chocolate(small, big, goal):
+    big *= 5
+    if big + small < goal or small < goal % 5:
+        return -1
+    small = goal - big
+    return small % 5 if small < 0 else small
